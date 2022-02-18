cf758232c2fcc678dc6a438139ef2e171acfdd52
Matt Martin <mtkmartin@gmail.com>
2022-02-18 14:44:46 -0700

    Adding multi-steal dialog fix

--- bug_fixes/capture.py
+++ bug_fixes/capture.py
@@ -12,11 +12,73 @@ class Capture:
         # Fixes issue with multiple steals caused by Genji Glove and/or Offering Capture.
         # Issues resolved:
         #    1) the stolen items are not all added to your inventory (only the last successful steal is actually added)
+        #    2) the message display window does not clear in between steal animations, 
+        #        meaning that the first item name is the one that is displayed for all subsequent successful steals. 
         # Based in part on https://www.angelfire.com/al2/imzogelmo/patches.html#patches's Multi-Steal Fix
-        # Code actually derived from Bropedio's Multi-Steal fix: 
-        #  https://www.ff6hacking.com/forums/thread-4124-post-40232.html#pid40232
-        # Known remaining issue: the message display window does not clear in between steal animations, 
-        #  meaning that the first item name is the one that is displayed for all subsequent successful steals. 
+        #       and Bropedio's Multi-Steal fix (https://www.ff6hacking.com/forums/thread-4124-post-40232.html#pid40232)
+        
+        # Custom variable locations
+        STOLEN_ITEM_ARRAY_START = 0x2f35
+        STOLEN_ITEM_ARRAY_INDEX = 0x2f3b
+
+        # Make the "Steal <item>" text go through the array
+        src = [
+            asm.LDA(0x2f35, asm.ABS),   #displaced instruction
+            asm.PHA(),                  #save A
+            asm.REP(0x20),              #Set A to 16 bits
+            asm.LDA(0x76, asm.DIR_16),  #Load first two bytes of current animation entry 
+            asm.CMP(0x0302, asm.IMM16), #Check for animation opcode 2 (upper text box) and text message 3 (Steal <item>)
+            asm.SEP(0x20),              #Set A back to 8 bits 
+            asm.BEQ("GET_STEAL_ITEM"),  #If the above condition was true, branch
+            asm.PLA(),                  #Else, Restore A
+            asm.RTS(),                  #      and return
+            "GET_STEAL_ITEM",           
+            asm.SEP(0x10),              #Set X to 8 bits
+            asm.PLA(),                  # Restore A if we took this branch
+            asm.LDX(STOLEN_ITEM_ARRAY_INDEX, asm.ABS),   # Load the index to the stolen item array
+            asm.LDA(STOLEN_ITEM_ARRAY_START, asm.ABS_X), # Put the item from index into A
+            asm.REP(0x10),              # Set X back to 16 bits
+            asm.INC(STOLEN_ITEM_ARRAY_INDEX, asm.ABS),   # Increment the array index
+            asm.RTS()
+        ]
+        space = Write(Bank.C1, src, "Multisteal Fix: steal <item> text")
+        c1_steal_print_addr = space.start_address
+
+        # Call our new subroutine
+        space = Reserve(0x15f06, 0x15f08, "Multisteal Fix: call new C1 subroutine to load stolen item into A", asm.NOP())
+        space.write(
+            asm.JSR(c1_steal_print_addr, asm.ABS)
+        )
+
+        #These two subroutines reset the stolen item index
+        src = [
+            asm.JSR(0x1429, asm.ABS),                   # displaced instruction
+            asm.STZ(STOLEN_ITEM_ARRAY_INDEX, asm.ABS),  # zero the index
+            asm.RTS()
+        ]
+        space = Write(Bank.C2, src, "Multisteal fix: reset stolen item array index routine")
+        stolen_item_index_reset = space.start_address
+
+        space = Reserve(0x2140f, 0x21411, "Multisteal Fix: reset stolen item index")
+        space.write(
+            asm.JSR(stolen_item_index_reset, asm.ABS)
+        )
+
+        src = [
+            asm.LDA(0xb5, asm.DIR),                     # displaced instruction
+            asm.ASL(),                                  # displaced instruction
+            asm.STZ(STOLEN_ITEM_ARRAY_INDEX, asm.ABS),  # zero the index
+            asm.RTS()
+        ]
+        space = Write(Bank.C2, src, "Multisteal fix: reset stolen item array index routine")
+        stolen_item_index_reset = space.start_address
+
+        space = Reserve(0x213fa, 0x213fc, "Multisteal Fix: reset stolen item index")
+        space.write(
+            asm.JSR(stolen_item_index_reset, asm.ABS)
+        )
+
+
 
         # New subroutine for storing acquired item
         src = [
@@ -25,6 +87,13 @@ class Capture:
             asm.PHA(),                  # save reserve item on stack
             asm.XBA(),                  # get new item in A
             asm.STA(0x32f4, asm.ABS_X), # store new item in reserve byte
+            # Store item in array for textbox
+            asm.PHX(),                  # save X
+            asm.LDX(STOLEN_ITEM_ARRAY_INDEX, asm.ABS),   # Load the index for the array
+            asm.STA(STOLEN_ITEM_ARRAY_START, asm.ABS_X), # Store the item number into the array
+            asm.INC(STOLEN_ITEM_ARRAY_INDEX, asm.ABS),   # Increment the index for highest variable stored
+            asm.PLX(),                  # restore X
+            # Done storing item in array for textbox
             asm.PHX(),                  # save X
             asm.JSR(0x62C7, asm.ABS),   # add reserve to obtained-items buffer
             asm.PLX(),                  # restore X
@@ -36,7 +105,7 @@ class Capture:
         store_acquired_addr = space.start_address
 
         # Update steal formula where it stores the acquired item
-        space = Reserve(0x239ec, 0x239f4, "Multisteal Fix: call new subroutine", asm.NOP())
+        space = Reserve(0x239e9, 0x239f4, "Multisteal Fix: call new subroutine", asm.NOP())
         space.write(
             asm.XBA(),                             # store acquired item in B
             asm.LDA(0x3018, asm.ABS_X),            # character's unique bit
