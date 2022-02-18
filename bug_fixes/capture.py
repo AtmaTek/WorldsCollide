077eb006ba958548354bb85b5c0465684e7f9b54
Matt Martin <mtkmartin@gmail.com>
2022-02-18 09:36:08 -0700

    Adding Bropedio's Multi-Steal fix

--- bug_fixes/capture.py
+++ bug_fixes/capture.py
@@ -5,9 +5,51 @@ import args
 class Capture:
     def __init__(self):
         if args.fix_capture:
-            self.mod()
+            self.weapon_special_mod()
+            self.multisteal_mod()
 
-    def mod(self):
+    def multisteal_mod(self):
+        # Fixes issue with multiple steals caused by Genji Glove and/or Offering Capture.
+        # Issues resolved:
+        #    1) the stolen items are not all added to your inventory (only the last successful steal is actually added)
+        # Based in part on https://www.angelfire.com/al2/imzogelmo/patches.html#patches's Multi-Steal Fix
+        # Code actually derived from Bropedio's Multi-Steal fix: 
+        #  https://www.ff6hacking.com/forums/thread-4124-post-40232.html#pid40232
+        # Known remaining issue: the message display window does not clear in between steal animations, 
+        #  meaning that the first item name is the one that is displayed for all subsequent successful steals. 
+
+        # New subroutine for storing acquired item
+        src = [
+            asm.TSB(0x3a8c, asm.ABS),   # set character's reserve item to be added
+            asm.LDA(0x32f4, asm.ABS_X), # load current reserve item
+            asm.PHA(),                  # save reserve item on stack
+            asm.XBA(),                  # get new item in A
+            asm.STA(0x32f4, asm.ABS_X), # store new item in reserve byte
+            asm.PHX(),                  # save X
+            asm.JSR(0x62C7, asm.ABS),   # add reserve to obtained-items buffer
+            asm.PLX(),                  # restore X
+            asm.PLA(),                  # restore previous reserve item
+            asm.STA(0x32f4, asm.ABS_X), # store in reserve item byte again
+            asm.RTS()
+        ]
+        space = Write(Bank.C2, src, "Multisteal Fix: store acquired item")
+        store_acquired_addr = space.start_address
+
+        # Update steal formula where it stores the acquired item
+        space = Reserve(0x239ec, 0x239f4, "Multisteal Fix: call new subroutine", asm.NOP())
+        space.write(
+            asm.XBA(),                             # store acquired item in B
+            asm.LDA(0x3018, asm.ABS_X),            # character's unique bit
+            asm.JSR(store_acquired_addr, asm.ABS), # save new item to buffer
+        )
+
+        # Fix Item Return Buffer
+        space = Reserve(0x112d5, 0x112d7, "Multisteal Fix: avoid item return buffer overrun")
+        space.write(
+            asm.CPX(0x50, asm.IMM16) # the game only clears #$40 for item buffer, but it expects #$50
+        )
+
+    def weapon_special_mod(self):
         # http://assassin17.brinkster.net/patches.htm#anchor18
         NEW_SPECIAL_EFFECT_VAR = 0x2f3d
 
@@ -91,3 +133,4 @@ class Capture:
             asm.JSR(dice_toss_animation_addr, asm.ABS), #Jump to our new routine
             asm.RTS()                                   #Done
         )
+
