f4f363ad26e12e50ac1e094ae4833a814ccc7cf4
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-04-16 18:31:43 -0400

    remove dbugpy code

--- wc.py
+++ wc.py
@@ -26,10 +26,5 @@ def main():
     data.write()
     memory.write()
 
-
-import debugpy
-debugpy.listen(5678)
-debugpy.wait_for_client()  # blocks execution until client is attached
-
 if __name__ == '__main__':
     main()
