aaeba39edf51428c332f0ea7410a407d176a9a64
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-04-16 18:27:29 -0400

    add boilerplate for adding future checks

--- wc.py
+++ wc.py
@@ -26,5 +26,10 @@ def main():
     data.write()
     memory.write()
 
+
+import debugpy
+debugpy.listen(5678)
+debugpy.wait_for_client()  # blocks execution until client is attached
+
 if __name__ == '__main__':
     main()
