861618f5d134824779b8a17d8f7c6e0c5b72dc59
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-05-06 15:40:02 -0400

    - only apply owzers fix logic to bdash/shoebdash - move movement speed constants to data/movement

--- data/movement.py
+++ data/movement.py
@@ -1,15 +1,25 @@
-# Terminology
-#   WALK   - movespeed 3
-#   SPRINT - movespeed 4
-#   DASH   - movespeed 5
-class MovementActions:
-    # WALK by default, SPRINT with sprint shoes equipped
+
+from enum import IntEnum
+
+
+class MovementSpeed:
+    WALK = 2    # Original FF6 walk speed
+    SPRINT = 3  # Original FF6 sprint speed
+    DASH = 4    # Custom, move twice as fast as sprint.
+
+class MovementActions(IntEnum):
+    # WALK by default
+    # SPRINT with sprint shoes equipped
     ORIGINAL = 'og'
-    # SPRINT by default, WALK when holding B
+    # SPRINT by default
+    # WALK when holding B
     AUTO_SPRINT = 'as'
-    # SPRINT by default, DASH when holding B
+    # SPRINT by default
+    # DASH when holding B
     B_DASH = 'bd'
-    # SPRINT by default, DASH when holding B with sprint shoes equipped
+    # SPRINT by default
+    # DASH when holding B with sprint shoes equipped
+    # WALK when holding B without sprint shoes equipped
     SPRINT_SHOES_B_DASH = 'ssbd'
 
     ALL = [ORIGINAL, AUTO_SPRINT, B_DASH, SPRINT_SHOES_B_DASH]
