d6df4eb791ab91d65a699c5ee71c7d0583e7e317
Matt Martin <mtkmartin@gmail.com>
2022-05-16 21:16:46 -0600

    Adding logic to display Special rage names

--- menus/rage.py
+++ menus/rage.py
@@ -1,8 +1,6 @@
 from memory.space import Bank, Reserve, Allocate, Write
 import instruction.asm as asm
 
-# Testing with 
-# py wc.py -i ../rom/ff3.sfc -sc1 terra -com 16989898989898989898989898 -srr 255 255
 class RageMenu:
     def __init__(self, rages):
         self.rages = rages
@@ -11,6 +9,7 @@ class RageMenu:
 
     def draw_ability_names_mod(self):
         import data.text as text
+        from data.spell_names import name_id
 
         rage_data_address = self.rages.ATTACKS_DATA_START + 0xc00000
 
@@ -27,12 +26,12 @@ class RageMenu:
         space = Write(Bank.C3, src, "draw comma & space")
         draw_comma_space = space.start_address
 
-        # argument: x = rage data index
+        # argument: a = rage ability ID
         # returns:  y = length of string, x = pointer to string
         # Based in part on https://github.com/FF6BeyondChaos/BeyondChaosRandomizer/blob/082695ebcb7e817dee5b2a778a4620963af5a4df/BeyondChaos/menufeatures.py#L77
-        index_in_ram = 0x38 # RAM variable
+        ability_id_in_ram = 0x38      # RAM variable (1 byte)
+        rage_idx_in_ram = 0xE0        # RAM variable (2 bytes)
         src = [
-            asm.LDA(rage_data_address, asm.LNG_X),   # a = ability id
             asm.CMP(0x36, asm.IMM8),                 # less than 54? It's a spell with length 7
             asm.BCC("GET_7_CHAR_SPELL_NAME"),
             asm.CMP(0x51, asm.IMM8),                 # else if less than 81? It's an esper with length 8
@@ -43,11 +42,12 @@ class RageMenu:
             asm.SBC(0x51, asm.IMM8),
             asm.A16(),
             asm.AND(0x00ff, asm.IMM16),
-            asm.STA(index_in_ram, asm.ABS), 
+            asm.STA(ability_id_in_ram, asm.ABS), 
             asm.ASL(),
             asm.ASL(),
-            asm.ADC(index_in_ram, asm.ABS),
-            asm.ASL(),
+            asm.CLC(),
+            asm.ADC(ability_id_in_ram, asm.ABS),
+            asm.ASL(), # A *= 10
             asm.ADC(0x252, asm.IMM16),  # start of attack name - 0x26f567
             asm.TAX(),
             asm.LDY(0x000a, asm.IMM16),  # 10 characters
@@ -56,12 +56,12 @@ class RageMenu:
             "GET_7_CHAR_SPELL_NAME",
             asm.A16(),
             asm.AND(0x00ff, asm.IMM16),
-            asm.STA(index_in_ram, asm.ABS), 
+            asm.STA(ability_id_in_ram, asm.ABS), 
             asm.ASL(),
             asm.ASL(),
             asm.ASL(),  # A *= 8
             asm.SEC(),
-            asm.SBC(index_in_ram, asm.DIR), # change it to A *= 7
+            asm.SBC(ability_id_in_ram, asm.DIR), # change it to A *= 7
             asm.TAX(),
             asm.LDY(0x0007, asm.IMM16),  # 7 character length
             asm.A8(),
@@ -88,8 +88,34 @@ class RageMenu:
             asm.PHX(),
             asm.PHY(),
 
+            # "Special" ability logic to get the name of the special ability
+            asm.LDA(rage_data_address, asm.LNG_X), # a = ability id
+            asm.CMP(name_id["Special"], asm.IMM8), # is this a Special ability?
+            asm.BNE("GET_ABILITY_NAME"),           # if not, get the name of the ability
+            # else, get the Special name instead from 0fd0d0 + rage_idx * 10, length 10
+            asm.LDY(0x000a, asm.IMM16),  # 10 characters
+            asm.LDA(rage_idx_in_ram, asm.ABS), # rage_idx
+            asm.A16(),
+            asm.AND(0x00ff, asm.IMM16),
+            asm.ASL(),
+            asm.ASL(),
+            asm.CLC(),
+            asm.ADC(rage_idx_in_ram, asm.ABS),
+            asm.ASL(), # A *= 10
+            asm.TAX(),
+            asm.A8(),
+            "SPECIAL_NAME_LOOP_START",
+            # x = offset from start of special names (0fd0d0), y = length (10)
+            asm.LDA(0x0fd0d0, asm.LNG_X),           # a = current char in special name
+            asm.CMP(space_value, asm.IMM8),         # compare with character ' ' which pads end of ability names
+            asm.BEQ("DRAW_ABILITY_NAME_RETURN"),    # branch if reached end of special name
+            asm.STA(0x2180, asm.ABS),               # add character to string
+            asm.INX(),                              # next character in special name
+            asm.DEY(),                              # decrement special name character index
+            asm.BNE("SPECIAL_NAME_LOOP_START"),     # branch if not zero
+            asm.BRA("DRAW_ABILITY_NAME_RETURN"),    # else, done
+            "GET_ABILITY_NAME",
             asm.JSR(get_ability_name, asm.ABS),
-
             # x = offset from start of ability names (26f567), y = length
             "ABILITY_NAME_LOOP_START",
             asm.LDA(0x26f567, asm.LNG_X),           # a = current char in ability name
@@ -99,7 +125,6 @@ class RageMenu:
             asm.INX(),                              # next character in ability name
             asm.DEY(),                              # decrement ability name character index
             asm.BNE("ABILITY_NAME_LOOP_START"),     # branch if not zero
-
             "DRAW_ABILITY_NAME_RETURN",
             asm.PLY(),
             asm.PLX(),
@@ -120,6 +145,7 @@ class RageMenu:
             asm.BEQ("END_STRING_RETURN"),   # branch if rage at cursor index not learned
 
             asm.A16(),
+            asm.STA(rage_idx_in_ram, asm.ABS), # store the rage_idx, as it aligns with the monster index elsewhere to lookup Special abilities
             asm.ASL(),                      # a = rage index * 2 (2 abilities per rage)
             asm.TAX(),                      # x = rage index * 2 (rage ability data index)
             asm.TDC(),                      # clear A of any 16-bit remnants
