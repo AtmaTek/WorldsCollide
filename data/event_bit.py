47669fe1d613ac86e8e42640b89f4335f13dc663
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-05-27 13:06:25 -0400

    move bit for perma skip

--- data/event_bit.py
+++ data/event_bit.py
@@ -202,8 +202,13 @@ TEMP_SONG_OVERRIDE = 0x1cc
 ENABLE_Y_PARTY_SWITCHING = 0x1ce
 ALWAYS_CLEAR = 0x176 # this event_bit is always clear, used for branching
 
-# bits 0x1e6-0x1ed  Unused, as the SNES versions feature 20 rare item slots rather than 30
-UNLOCKED_PERMA_KT_SKIP = 0x1e6 # custom
+# Unused Bits
+# bits 0x200-0x22e Used for banquet soldiers
+# 2 bits 0x1bc-0x1bd Unused
+# 3 bits 0x1c7-0x1c9 Unused
+# 3 bits 0x2c1-0x2c3 Unused
+UNLOCKED_PERMA_KT_SKIP = 0x2c1
+# 8 bits 0x1e6-0x1ed Unused, as the SNES versions feature 20 rare item slots rather than 30
 
 from constants.objectives import MAX_OBJECTIVES
 for index in range(MAX_OBJECTIVES):
