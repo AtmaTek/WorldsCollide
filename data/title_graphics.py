2ecd4b94ac0ca53e46c991b2229d67f0c7a67da2
Matt Martin <mtkmartin@gmail.com>
2022-06-29 19:37:36 -0600

    ensuring title screen file gets closed

--- data/title_graphics.py
+++ data/title_graphics.py
@@ -6,14 +6,14 @@ class TitleGraphics:
 
     def mod(self):
         # Read in the title graphics bin and write it to 18f000 - 194e95
-        binFile = open('graphics/title/WC Spartan Title Data-CDude.bin', "rb")
-        bin = binFile.read(-1)
-        space = Reserve(0x18f000, 0x194e95, "title graphics (compressed)")
+        with open('graphics/title/WC Spartan Title Data-CDude.bin', "rb") as binFile:
+            data = binFile.read()
 
-        if len(space) != len(bin):
-            raise ValueError(f"Invalid title graphics bin size ({len(bin)} should be {len(space)})")
+            space = Reserve(0x18f000, 0x194e95, "title graphics (compressed)")
+            if len(space) != len(data):
+                raise ValueError(f"Invalid title graphics bin size ({len(data)} should be {len(space)})")
 
-        space.write(bin)
+            space.write(data)
 
     def write(self):
         if self.args.spoiler_log:
