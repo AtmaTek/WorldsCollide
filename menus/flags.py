56f050eb0b79e7e8a3a7a4b90ca5201dd3a551e9
Matt Martin <mtkmartin@gmail.com>
2022-08-17 19:21:06 -0600

    Adding -hf flag to hide flags for fun mystery seeds

--- menus/flags.py
+++ menus/flags.py
@@ -10,25 +10,28 @@ class Flags(scroll_area.ScrollArea):
 
     def __init__(self):
         self.lines = []
-        for _, group in args.group_modules.items():
-            if hasattr(group, "menu"):
-                name, options = group.menu(args)
+        if args.hide_flags:
+            self.lines.append(scroll_area.Line("Flags Hidden", f0.set_blue_text_color))
+        else:
+            for _, group in args.group_modules.items():
+                if hasattr(group, "menu"):
+                    name, options = group.menu(args)
 
-                self.lines.append(scroll_area.Line(name, f0.set_blue_text_color))
-                for option in options:
-                    key, value = option
+                    self.lines.append(scroll_area.Line(name, f0.set_blue_text_color))
+                    for option in options:
+                        key, value = option
 
-                    key = "  " + key.replace("&", "+")
-                    value = str(value)
-                    if value == "True":
-                        value = "T"
-                    elif value == "False":
-                        value = "F"
+                        key = "  " + key.replace("&", "+")
+                        value = str(value)
+                        if value == "True":
+                            value = "T"
+                        elif value == "False":
+                            value = "F"
 
-                    padding = scroll_area.WIDTH - (len(key) + len(value))
-                    self.lines.append(scroll_area.Line(f"{key}{' ' * padding}{value}", f0.set_user_text_color))
+                        padding = scroll_area.WIDTH - (len(key) + len(value))
+                        self.lines.append(scroll_area.Line(f"{key}{' ' * padding}{value}", f0.set_user_text_color))
 
-                self.lines.append(scroll_area.Line("", f0.set_user_text_color))
-        del self.lines[-1] # exclude final empty line
+                    self.lines.append(scroll_area.Line("", f0.set_user_text_color))
+            del self.lines[-1] # exclude final empty line
 
         super().__init__()
