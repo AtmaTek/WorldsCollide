f88c435636e695affc193bfa7e56ca4d26c09673
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-11-03 18:03:08 -0400

    more cleanup

--- build-wc-flag-metadata.py
+++ build-wc-flag-metadata.py
@@ -3,9 +3,9 @@ def main():
     from metadata.flag_metadata_writer import FlagMetadataWriter
     FlagMetadataWriter(args).write()
 
-import debugpy
-debugpy.listen(5678)
-debugpy.wait_for_client()  # blocks execution until client is attached
+# import debugpy
+# debugpy.listen(5678)
+# debugpy.wait_for_client()  # blocks execution until client is attached
 
 if __name__ == '__main__':
     main()
