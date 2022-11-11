dad6ec443c758f3c12cdea9a9bbf9104e59c1104
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-11-11 14:29:33 -0500

    allow for pose_id

--- graphics/tools/print_sprite.py
+++ graphics/tools/print_sprite.py
@@ -1,11 +1,11 @@
 
-def print_sprite(sprite_id, palette_id):
+def print_sprite(sprite_id, palette_id, pose_id):
     from graphics.sprites.sprites import get_path as get_sprite_path
     from graphics.palettes.palettes import  get_path as get_palette_path
 
-    print(str(get_rgb_bytes(get_sprite_path(sprite_id), get_palette_path(palette_id))))
+    print(str(get_rgb_bytes(get_sprite_path(sprite_id), get_palette_path(palette_id), pose_id)))
 
-def get_rgb_bytes(sprite_path, palette_path):
+def get_rgb_bytes(sprite_path, palette_path, pose_id):
     from graphics.palette_file import PaletteFile
     from graphics.sprite_file import SpriteFile
     from graphics.poses import CHARACTER
@@ -13,7 +13,7 @@ def get_rgb_bytes(sprite_path, palette_path):
 
     sprite = SpriteFile(sprite_path, palette)
 
-    return sprite.rgb_data(CHARACTER[1])
+    return sprite.rgb_data(CHARACTER[pose_id])
 
 if __name__ == "__main__":
     import os, sys
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("sprite_id", type = int, help = "Id of the sprite to print")
     parser.add_argument("palette_id", type = int, help = "Id of the palette for the sprite")
+    parser.add_argument("pose_id", type = int, help = "Id of the pose for the sprite", required=False, default=1)
 
     args = parser.parse_args()
 
-    print_sprite(args.sprite_id, args.palette_id)
+    print_sprite(args.sprite_id, args.palette_id, args.pose_id)
