6c0c9abd1f1d5de263c6ca01f0494f552f4aa189
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-12-02 16:10:29 -0500

    remove unwanted presets

--- constants/check_presets.py
+++ constants/check_presets.py
@@ -14,26 +14,6 @@ AH_CLOSED = CheckPreset(
     ]
 )
 
-SEDENTARY_LIFESTYLE = CheckPreset(
-    "sitlife",
-    "Sedentary Lifestyle",
-    RewardType.ITEM,
-    "Longer checks in the game are guaranteed to reward an ITEM.",
-    [
-        ANCIENT_CASTLE,
-        BURNING_HOUSE,
-        PHOENIX_CAVE,
-        FLOATING_CONT_ARRIVE,
-        FLOATING_CONT_BEAST,
-        FLOATING_CONT_ESCAPE,
-        LETE_RIVER,
-        MAGITEK_FACTORY_FINISH,
-        OPERA_HOUSE_DISRUPTION,
-        PHANTOM_TRAIN,
-        FANATICS_TOWER_FOLLOWER,
-    ],
-)
-
 NO_FREE_CHARACTERS = CheckPreset(
     'nfchar',
     "No Free Characters",
@@ -50,7 +30,7 @@ NO_FREE_CHARACTERS = CheckPreset(
     ]
 )
 
-NO_FREE_PROGRESSION = CheckPreset(
+NO_FREE_CHARACTERS_ESPERS = CheckPreset(
     'nfce',
     "No Free C+E",
     RewardType.ITEM,
@@ -71,62 +51,10 @@ NO_FREE_PROGRESSION = CheckPreset(
     ]
 )
 
-NO_FREE_PROGRESSION_NO_GP = CheckPreset(
-    'nfcegp',
-    "No Free C+E (No AH/Tzen)",
-    RewardType.ITEM,
-    "All free checks are guaranteed to reward an ITEM. GP-based checks Auction House and Tzen Thief can still reward ESPER or ITEM.",
-    [
-        COLLAPSING_HOUSE,
-        FIGARO_CASTLE_THRONE,
-        GAUS_FATHERS_HOUSE,
-        KOHLINGEN_CAFE,
-        MT_ZOZO,
-        NARSHE_WEAPON_SHOP,
-        NARSHE_WEAPON_SHOP_MINES,
-        SEALED_GATE,
-        SOUTH_FIGARO_PRISONER,
-    ]
-)
-
-SQUID_GAMES = CheckPreset(
-    'squid',
-    'Squid Games',
-    RewardType.CHARACTER,
-    'The checks which you fight Ultros in the vanilla game are guaranteed to reward a CHARACTER. The checks are Lete River, Opera House, Esper Mountain, and Floating Continent Arrival.',
-    [
-        ESPER_MOUNTAIN,
-        FLOATING_CONT_ARRIVE,
-        LETE_RIVER,
-        OPERA_HOUSE_DISRUPTION,
-    ]
-)
-
-VANILLA_ESPERS = CheckPreset(
-    'vanesp',
-    "Vanilla Espers",
-    RewardType.ESPER,
-    "Most checks/events in the vanilla game which revolve around espers are guaranteed to reward an ESPER.",
-    [
-        ANCIENT_CASTLE,
-        MAGITEK_FACTORY_TRASH,
-        MAGITEK_FACTORY_GUARD,
-        MAGITEK_FACTORY_FINISH,
-        SEALED_GATE,
-        ESPER_MOUNTAIN,
-        PHOENIX_CAVE,
-        ZOZO_TOWER,
-    ]
-)
-
 all_presets = [
     AH_CLOSED,
     NO_FREE_CHARACTERS,
-    NO_FREE_PROGRESSION,
-    NO_FREE_PROGRESSION_NO_GP,
-    SEDENTARY_LIFESTYLE,
-    SQUID_GAMES,
-    VANILLA_ESPERS,
+    NO_FREE_CHARACTERS_ESPERS,
 ]
 
 preset_keys = [preset.key for preset in all_presets]
