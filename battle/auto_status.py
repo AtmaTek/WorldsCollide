42cb7e7d8f6933c950516452823bad0d8019e879
asilverthorn <96998881+asilverthorn@users.noreply.github.com>
2022-12-18 14:41:46 -0700

    Adding Auto Life 3 and Auto Dog Block objective results (#36)

--- battle/auto_status.py
+++ battle/auto_status.py
@@ -9,12 +9,15 @@ class _AutoStatus:
     def __init__(self):
         auto_b_status_effects = ["Condemned", "Image", "Mute", "Berserk", "Muddle", "Seizure", "Sleep"]
         auto_c_status_effects = ["Float", "Regen", "Slow", "Haste", "Shell", "Safe", "Reflect"]
+        auto_d_status_effects = ["Life 3", "Dog Block"]
 
         auto_addresses = []
         for status in auto_b_status_effects:
             auto_addresses.append(self.auto_status(status, status_effects.B))
         for status in auto_c_status_effects:
             auto_addresses.append(self.auto_status(status, status_effects.C))
+        for status in auto_d_status_effects:
+            auto_addresses.append(self.auto_status(status, status_effects.D))
 
         src = [
             # original replaced code
@@ -40,8 +43,11 @@ class _AutoStatus:
             asm.JSL(START_ADDRESS_SNES + auto_status_effects),
         )
 
+        # Ensure that Life 3 can also be applied at the start of battle
+        space = Reserve(0x22823, 0x22823, "Only keep Dog Block and Float")
+        space.write(0xC4) # Original: 0xC0; also keeping Life 3
+
     def auto_status(self, status_name, status_effects_group):
-        status_name = status_name.capitalize()
         auto_status_name = "Auto " + status_name
         auto_status_name_upper = auto_status_name.upper()
 
@@ -51,8 +57,13 @@ class _AutoStatus:
             status_bit = 1 << status_effects_group.name_id[status_name]
         if status_effects_group == status_effects.B:
             status_address = 0x3c6c
+            opcode = asm.ABS_X
         elif status_effects_group == status_effects.C:
             status_address = 0x3c6d
+            opcode = asm.ABS_X
+        elif status_effects_group == status_effects.D:
+            status_address = 0x1615 
+            opcode = asm.ABS_Y
 
         src = []
         if auto_status_name in objectives.results:
@@ -71,9 +82,26 @@ class _AutoStatus:
             asm.RTS(),
 
             auto_status_name_upper,
-            asm.LDA(status_address, asm.ABS_X),
+        ]
+        
+        # if the opcode is Y, that means we're accessing the SRAM offset, for which Y is multiples of 37
+        if(opcode == asm.ABS_Y):
+            src += [
+                asm.PHY(), # push current Y
+                asm.XY16(), # 16-bit X & Y
+                asm.LDY(0x3010, asm.ABS_X) # get the pointer to the character
+            ]
+        src += [
+            asm.LDA(status_address, opcode),
             asm.ORA(status_bit, asm.IMM8),
-            asm.STA(status_address, asm.ABS_X),
+            asm.STA(status_address, opcode),
+        ]
+        if(opcode == asm.ABS_Y):
+            src += [
+                asm.XY8(), # revert back to 8-bit X&Y
+                asm.PLY(), # pull original Y
+            ]
+        src += [
             asm.RTS(),
         ]
         space = Write(Bank.F0, src, auto_status_name)
