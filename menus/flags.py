0bfde9d1e5a5f2f8fb65302e53540dc752bc51f3
FF6 Worlds Collide <115676184+ff6wc@users.noreply.github.com>
2022-12-19 00:20:30 -0500

    Update flags.py
    Updating to match dev branch

--- menus/flags.py
+++ menus/flags.py
@@ -12,31 +12,34 @@ class Flags(scroll_area.ScrollArea):
     def __init__(self):
         self.lines = []
         self.submenus = {} # dictionary of submenus. key = line number, value = ScrollArea derived class
-        for _, group in args.group_modules.items():
-            if hasattr(group, "menu"):
-                name, options = group.menu(args)
+        if args.hide_flags:
+            self.lines.append(scroll_area.Line("Flags Hidden", f0.set_blue_text_color))
+        else:
+            for _, group in args.group_modules.items():
+                if hasattr(group, "menu"):
+                    name, options = group.menu(args)
 
-                self.lines.append(scroll_area.Line(name, f0.set_blue_text_color))
-                for option in options:
-                    key, value = option
+                    self.lines.append(scroll_area.Line(name, f0.set_blue_text_color))
+                    for option in options:
+                        key, value = option
 
-                    key = "  " + key.replace("&", "+")
+                        key = "  " + key.replace("&", "+")
 
-                    # if we're given a scroll area, save it as a sub-menu with a value of X …, where X is the number of items in the sub-menu
-                    if isinstance(value, scroll_area.ScrollArea):
-                        self.submenus[len(self.lines)] = value
-                        value = f"{value.number_items} {chr(text_value['…'])}"
+                        # if we're given a scroll area, save it as a sub-menu with a value of X …, where X is the number of items in the sub-menu
+                        if isinstance(value, scroll_area.ScrollArea):
+                            self.submenus[len(self.lines)] = value
+                            value = f"{value.number_items} {chr(text_value['…'])}"
 
-                    value = str(value)
-                    if value == "True":
-                        value = "T"
-                    elif value == "False":
-                        value = "F"
+                        value = str(value)
+                        if value == "True":
+                            value = "T"
+                        elif value == "False":
+                            value = "F"
 
-                    padding = scroll_area.WIDTH - (len(key) + len(value))
-                    self.lines.append(scroll_area.Line(f"{key}{' ' * padding}{value}", f0.set_user_text_color))
+                        padding = scroll_area.WIDTH - (len(key) + len(value))
+                        self.lines.append(scroll_area.Line(f"{key}{' ' * padding}{value}", f0.set_user_text_color))
 
-                self.lines.append(scroll_area.Line("", f0.set_user_text_color))
-        del self.lines[-1] # exclude final empty line
+                    self.lines.append(scroll_area.Line("", f0.set_user_text_color))
+            del self.lines[-1] # exclude final empty line
 
         super().__init__()
